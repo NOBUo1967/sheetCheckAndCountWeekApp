3161aacea9c276aa8baede18014316b334e3d3b8
Shin H <nobunegi13@gmail.com>
2021-10-18 18:07:40 +0900

    TabViewの実装を行った

--- sheetCheckAndCountWeekApp/Views/SheetCount.swift
+++ sheetCheckAndCountWeekApp/Views/SheetCount.swift
@@ -1,5 +1,5 @@
 //
-//  ContentView.swift
+//  SheetCount.swift
 //  sheetCheckAndCountWeekApp
 //
 //  Created by Shinichiro Hirasawa on 2021/10/18.
@@ -7,15 +7,14 @@
 
 import SwiftUI
 
-struct ContentView: View {
+struct SheetCount: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        Text("シート計算ページです")
     }
 }
 
-struct ContentView_Previews: PreviewProvider {
+struct SheetCount_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        SheetCount()
     }
 }
---
+++ sheetCheckAndCountWeekApp/Views/WeekCount.swift
@@ -0,0 +1,20 @@
+//
+//  WeekCount.swift
+//  sheetCheckAndCountWeekApp
+//
+//  Created by Shinichiro Hirasawa on 2021/10/18.
+//
+
+import SwiftUI
+
+struct WeekCount: View {
+    var body: some View {
+        Text("週数カウントページです")
+    }
+}
+
+struct WeekCount_Previews: PreviewProvider {
+    static var previews: some View {
+        WeekCount()
+    }
+}
