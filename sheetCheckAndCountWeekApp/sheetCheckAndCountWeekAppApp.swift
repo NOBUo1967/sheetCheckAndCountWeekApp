f48f4c1921e23dfc703d5fe47cb872f958823cd3
Shin H <nobunegi13@gmail.com>
2021-10-18 17:32:32 +0900

    Initial Commit

---
+++ sheetCheckAndCountWeekApp/sheetCheckAndCountWeekAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  sheetCheckAndCountWeekAppApp.swift
+//  sheetCheckAndCountWeekApp
+//
+//  Created by Shinichiro Hirasawa on 2021/10/18.
+//
+
+import SwiftUI
+
+@main
+struct sheetCheckAndCountWeekAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
