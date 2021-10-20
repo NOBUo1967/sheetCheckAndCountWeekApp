ba85e3533a816b6987b7e7203a90b5acd0fdf4fc
Shin H <nobunegi13@gmail.com>
2021-10-21 06:41:13 +0900

    シート数計算ページ。Int型で定義しvalueを取得しようとしたが取得できなかった。

--- sheetCheckAndCountWeekApp/Views/SheetCount.swift
+++ sheetCheckAndCountWeekApp/Views/SheetCount.swift
@@ -7,11 +7,57 @@
 
 import SwiftUI
 
-struct SheetCount: View {
-    var body: some View {
-        Text("シート計算ページです")
+//extension UIApplication {
+//    func closeKeyboard() {
+//        sendAction(#selector(UIResponder.resignFirstResponder), to: nil, from: nil, for: nil)
+//    }
+//}
+
+extension UIApplication {
+    func endEditing() {
+        sendAction(#selector(UIResponder.resignFirstResponder),to: nil, from: nil,for: nil)
     }
 }
+
+struct SheetCount: View {
+    @State var dairyDose = 0
+    @State var numberOfDay = 0
+    @State var total = 0
+    
+    var body: some View {
+        VStack {
+            HStack {
+                Text("1日必要錠(包)数")
+                TextField("錠", value: self.$dairyDose, formatter: NumberFormatter()) // TextField
+                .textFieldStyle(RoundedBorderTextFieldStyle())
+                .keyboardType(.numberPad)
+            } // Hstack
+            .padding()
+            
+            HStack {
+                Text("日数")
+                TextField("日", value: self.$numberOfDay, formatter: NumberFormatter()) // TextField
+                .textFieldStyle(RoundedBorderTextFieldStyle())
+                .keyboardType(.numberPad)
+            } // Hstack
+            .padding()
+            
+            Button(action: {
+                print($dairyDose)
+                print($numberOfDay)
+                self.total = self.dairyDose * self.numberOfDay
+                UIApplication.shared.endEditing()
+                
+            }) {
+                Text("計算")
+            }
+            .padding()
+            Text("\(self.total)")
+        } // VStack
+    }
+}
+
+
 
 struct SheetCount_Previews: PreviewProvider {
     static var previews: some View {
