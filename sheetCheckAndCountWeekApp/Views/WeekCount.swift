ff70284f1a43a81b94f06df16d011e5e17087f9e
Shin H <nobunegi13@gmail.com>
2021-10-30 09:50:34 +0900

    DatePickerで日数を選択したらカレンダーが閉じるようにしたかったができなかった残渣

--- sheetCheckAndCountWeekApp/Views/WeekCount.swift
+++ sheetCheckAndCountWeekApp/Views/WeekCount.swift
@@ -7,21 +7,39 @@
 
 import SwiftUI
 
+//extension UIApplication {
+//    func dissmissKeyboard() {
+//        sendAction(#selector(UIResponder.resignFirstResponder),to: nil, from: nil,for: nil)
+//    }
+//}
+
 struct WeekCount: View {
-    @State var startDate = Date()
-    @State var endDate = Date()
+    @State private var startDate = Date()
+    @State private var endDate = Date()
+    
+//    func dissmissKeyboard() {
+//        UIApplication.shared.sendAction(#selector(UIResponder.resignFirstResponder), to: nil, from: nil, for: nil)
+//    }
+    
     
     var body: some View {
         Form {
             DatePicker("開始日", selection: $startDate, displayedComponents: .date)
                 .environment(\.locale, Locale(identifier: "ja_JP"))
+                .datePickerStyle(WheelDatePickerStyle())
+//                .onChange(of: startDate) { startDate in
+//                    print(startDate)
+//                }
+//                .onChange(of: startDate, perform: {
+//                    UIApplication.shared.dissmissKeyboard()
+//                })
             DatePicker("終了日", selection: $endDate, displayedComponents: .date)
                 .environment(\.locale, Locale(identifier: "ja_JP"))
             
             Text("original: \(startDate.description)")
             Text("original: \(endDate.description)")
-        }
-    }
+        } // Form
+    } // body
 }
 
 struct WeekCount_Previews: PreviewProvider {
