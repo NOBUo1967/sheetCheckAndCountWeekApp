659f75f5c98263e3a276ff1135b020a954677431
Shin H <nobunegi13@gmail.com>
2021-10-31 06:43:34 +0900

    [fix]指定した日付の時分秒を0にして日付の差の計算に影響が出ないように変更した。
    1976-04-01 23:59:00 +0000 と 1976-04-02 00:00:00 +0000で日付の計算をした際に1日差にならないパターンがあり、修正した。

--- sheetCheckAndCountWeekApp/Views/WeekCount.swift
+++ sheetCheckAndCountWeekApp/Views/WeekCount.swift
@@ -42,10 +42,25 @@ struct WeekCount: View {
         } // Form
     } // body
     
+    // 指定した日付の時間をリセットする関数
+    func resetTime(date: Date) -> Date {
+        let calendar: Calendar = Calendar(identifier: .gregorian)
+        var components = calendar.dateComponents([.year, .month, .day, .hour, .minute, .second], from: date)
+        
+        components.hour = 0
+        components.minute = 0
+        components.second = 0
+        
+        return calendar.date(from: components)!
+    }
+    
     // 日付の差を計算する関数
     func calclateSpan(startDate: Date, endDate: Date) -> Int {
+        let resetTimeStartDate = resetTime(date: startDate)
+        let resteTImeEndDate = resetTime(date: endDate)
+        
         // 計算部分 TimeIntervalがDouble型のため差spanはDouble型で宣言
-        let span:Double = endDate.timeIntervalSince(startDate)
+        let span:Double = resteTImeEndDate.timeIntervalSince(resetTimeStartDate)
         // TimeIntervalが秒数で与えられるため(60秒*60分*24時間)で除算して日数へ変換
         let differenceInTheNumberOfDays = span/(60*60*24)
         
