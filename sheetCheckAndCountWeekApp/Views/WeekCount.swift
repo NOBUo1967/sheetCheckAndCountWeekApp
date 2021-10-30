7387c123e7b827270d83b6f79634f6f0d7bc53d8
Shin H <nobunegi13@gmail.com>
2021-10-30 10:47:03 +0900

    [add]起算日から終了日の日数の差を計算するメソッドを定義した

--- sheetCheckAndCountWeekApp/Views/WeekCount.swift
+++ sheetCheckAndCountWeekApp/Views/WeekCount.swift
@@ -16,6 +16,7 @@ import SwiftUI
 struct WeekCount: View {
     @State private var startDate = Date()
     @State private var endDate = Date()
+//    @State private var span = Date()
     
 //    func dissmissKeyboard() {
 //        UIApplication.shared.sendAction(#selector(UIResponder.resignFirstResponder), to: nil, from: nil, for: nil)
@@ -37,8 +38,19 @@ struct WeekCount: View {
             
             Text("original: \(startDate.description)")
             Text("original: \(endDate.description)")
+            Text("\(startDate.description)から\(endDate.description)は\(calclateSpan(startDate: self.startDate, endDate: self.endDate))日")
         } // Form
     } // body
+    
+    // 日付の差を計算する関数
+    func calclateSpan(startDate: Date, endDate: Date) -> Int {
+        // 計算部分 TimeIntervalがDouble型のため差spanはDouble型で宣言
+        let span:Double = endDate.timeIntervalSince(startDate)
+        // TimeIntervalが秒数で与えられるため(60秒*60分*24時間)で除算して日数へ変換
+        let differenceInTheNumberOfDays = span/(60*60*24)
+        
+        return Int(differenceInTheNumberOfDays)
+    }
 }
 
 struct WeekCount_Previews: PreviewProvider {
