5fd2eedadc16059b867bd91ada1ac0483cd033b6
Shin H <nobunegi13@gmail.com>
2021-11-09 07:00:18 +0900

    [update]錠数入力部分や錠数表示部分の単位が錠と錠(包)で揃っていなかったため錠(包)に統一した

--- sheetCheckAndCountWeekApp/Views/SheetCount.swift
+++ sheetCheckAndCountWeekApp/Views/SheetCount.swift
@@ -30,7 +30,7 @@ struct SheetCount: View {
         VStack {
             HStack {
                 Text("1日必要錠(包)数")
-                TextField("錠", text: self.$dairyDose)
+                TextField("錠(包)", text: self.$dairyDose)
                     .textFieldStyle(RoundedBorderTextFieldStyle())
                     .keyboardType(.decimalPad) // TextField
             } // Hstack 1日必要錠(包)数
@@ -46,7 +46,7 @@ struct SheetCount: View {
             
             HStack {
                 Text("1シート(つづり)あたりの\n錠(包)数")
-                TextField("錠", text: self.$numberPerSheet)
+                TextField("錠(包)", text: self.$numberPerSheet)
                     .textFieldStyle(RoundedBorderTextFieldStyle())
                     .keyboardType(.numberPad) // TextField
             } // Hstack
@@ -72,7 +72,7 @@ struct SheetCount: View {
             } //Button
             .padding()
             // 端数は小数点第二位まで表示する。1回0.25錠までが現実的なところ。
-            Text("必要なヒートは\n\(self.total)シート\n+\(String(format: "%.2f", self.fraction))錠です")
+            Text("\(self.total)シート(つづり)\n+\(String(format: "%.2f", self.fraction))錠(包)\n必要です")
         } // VStack
         .alert(isPresented: $showAlert) {
             Alert(title: Text("正しい値を入力してください"), message: Text("入力できる値は数値のみです\n1シートあたりの錠数に０は入力できません"), dismissButton: .default(Text("OK")))
