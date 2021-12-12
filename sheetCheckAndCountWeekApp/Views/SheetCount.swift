2e07a2506942425900de013747c747ac558190fc
Shin H <nobunegi13@gmail.com>
2021-12-12 20:26:19 +0900

    [style]TextFieldのラベルに相当するText部分の文字サイズを変更。計算結果の数字部分に色を付けた。

--- sheetCheckAndCountWeekApp/Views/SheetCount.swift
+++ sheetCheckAndCountWeekApp/Views/SheetCount.swift
@@ -30,6 +30,7 @@ struct SheetCount: View {
         VStack {
             HStack {
                 Text("1日必要錠(包)数")
+                    .font(.title3)
                 // 両端寄せにするためにスペースを挿入
                 Spacer()
                 TextField("錠(包)", text: self.$dairyDose)
@@ -42,6 +43,7 @@ struct SheetCount: View {
             
             HStack {
                 Text("処方日数")
+                    .font(.title3)
                 // 両端寄せにするためにスペースを挿入
                 Spacer()
                 TextField("日", text: self.$numberOfDay)
@@ -54,6 +56,7 @@ struct SheetCount: View {
             
             HStack {
                 Text("1シート(つづり)あたりの\n錠(包)数")
+                    .font(.title3)
                 // 両端寄せにするためにスペースを挿入
                 Spacer()
                 TextField("錠(包)", text: self.$numberPerSheet)
@@ -63,7 +66,7 @@ struct SheetCount: View {
                 // TextField
             } // HStack シートあたりの錠数
             .padding()
-
+            
             
             Button(action: {
                 // キャストしてOptional型にしておかないとif let文で、nilチェックできないため、ここで値をDouble型にキャストしておく
@@ -71,9 +74,12 @@ struct SheetCount: View {
                 let numberOfDayDouble:Double? = Double(numberOfDay)
                 let numberPerSheetDouble: Double? = Double(numberPerSheet)
                 // 各値のnilチェックと空文字チェック
+                // 「 8」などスペース始まりの文字は弾いていないが、キーボードをNamPadに指定しているため問題ないと考える。
                 if let dairyDose = dairyDoseDouble, let numberOfDay = numberOfDayDouble, let numberPerSheet = numberPerSheetDouble, numberPerSheet != 0 {
+                    // 必要ヒート数を計算
                     self.total = Int(dairyDose * numberOfDay / numberPerSheet)
-                    self.fraction = (dairyDose * numberOfDay).truncatingRemainder(dividingBy: numberPerSheet)
+                    // 端数の錠数を計算。小数点第3位で四捨五入する
+                    self.fraction = round((dairyDose * numberOfDay).truncatingRemainder(dividingBy: numberPerSheet)*100) / 100
                 } else {
                     showAlert = true
                 }
@@ -84,7 +90,7 @@ struct SheetCount: View {
                     // buttonのデザインの部分
                     .fontWeight(.bold)
                     .foregroundColor(Color.white)
-                    .frame(width: 200, height: 30, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+                    .frame(width: 200, height: 30, alignment: .center)
                     .padding()
                     .background(Color.red)
                     .cornerRadius(10)
@@ -93,7 +99,11 @@ struct SheetCount: View {
             .frame(width: 300, height: 130, alignment: .center)
             
             // 端数は小数点第二位まで表示する。1回0.25錠までが現実的なところ。
-            Text("\(self.total)シート(つづり)\n+\(String(format: "%.2f", self.fraction))錠(包)\n必要です")
+            (Text("\(self.total)").foregroundColor(Color.red)
+                + Text("シート(つづり)\n+")
+                + Text("\(String(format: "%.2f", self.fraction))").foregroundColor(Color.red)
+                + Text("錠(包)\n必要です"))
+                .font(.title2)
         } // VStack
         .alert(isPresented: $showAlert) {
             Alert(title: Text("正しい値を入力してください"), message: Text("入力できる値は数値のみです\n1シートあたりの錠数に０は入力できません"), dismissButton: .default(Text("OK")))
