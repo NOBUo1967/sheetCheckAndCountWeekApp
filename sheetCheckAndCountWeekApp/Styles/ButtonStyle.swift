3d57ba130beb95844adb66390170260729b1e68a
Shin H <nobunegi13@gmail.com>
2022-01-05 06:51:54 +0900

    [update]ButtonStyleを定義した
    frameは各処で定義する必要があると考えたためbuttonStyleには含めなかった

---
+++ sheetCheckAndCountWeekApp/Styles/ButtonStyle.swift
@@ -0,0 +1,21 @@
+//
+//  ButtonStyle.swift
+//  sheetCheckAndCountWeekApp
+//
+//  Created by Shinichiro Hirasawa on 2022/01/05.
+//
+
+// commit_message:ButtonStyleを定義する
+
+import SwiftUI
+
+struct PrimaryButtonStyle: ButtonStyle {
+    func makeBody(configuration: Configuration) -> some View {
+        configuration.label
+            .padding()
+            .background(Color.red)
+            .foregroundColor(Color.white)
+            .font(.body.bold())
+            .cornerRadius(10)
+    }
+}
