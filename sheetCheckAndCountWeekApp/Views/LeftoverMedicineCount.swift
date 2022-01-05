d95a372bf2ba0893237c5413e3996b9927e3bfeb
Shin H <nobunegi13@gmail.com>
2022-01-06 06:53:35 +0900

    [add]WIP_init

---
+++ sheetCheckAndCountWeekApp/Views/LeftoverMedicineCount.swift
@@ -0,0 +1,22 @@
+//
+//  LeftoverMedicineCount.swift
+//  sheetCheckAndCountWeekApp
+//
+//  Created by Shinichiro Hirasawa on 2022/01/06.
+//
+
+// commit_message: WIP_init
+
+import SwiftUI
+
+struct LeftoverMedicineCount: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct LeftoverMedicineCount_Previews: PreviewProvider {
+    static var previews: some View {
+        LeftoverMedicineCount()
+    }
+}
