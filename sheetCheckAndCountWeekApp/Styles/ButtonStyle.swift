77b93286c3db2d5af66d34b45dca53d4bcb2c6c5
Shin H <nobunegi13@gmail.com>
2022-01-13 17:30:39 +0900

    [update]ButtonStyleに押下時のアニメーションを追加した

--- sheetCheckAndCountWeekApp/Styles/ButtonStyle.swift
+++ sheetCheckAndCountWeekApp/Styles/ButtonStyle.swift
@@ -5,12 +5,15 @@
 //  Created by Shinichiro Hirasawa on 2022/01/05.
 //
 
-// commit_message:ButtonStyleを定義する
+// commit_message:ButtonStyleに押下時のアニメーションを設定した
 
 import SwiftUI
 
 struct PrimaryButtonStyle: ButtonStyle {
     func makeBody(configuration: Configuration) -> some View {
+        //　押下しているか否かで動作を分けるための変数
+        let isPressed = configuration.isPressed
+        
         configuration.label
             .frame(width: 90, alignment: .center)
             .padding()
@@ -18,5 +21,10 @@ struct PrimaryButtonStyle: ButtonStyle {
             .foregroundColor(Color.white)
             .font(.body.bold())
             .cornerRadius(10)
+        // 押下時のボタンの見た目を変更
+            .scaleEffect(x: isPressed ? 0.9 : 1 , y: isPressed ? 0.9 : 1, anchor: .center)
+            .opacity(isPressed ? 0.3 : 1)
+        // 押下時のアニメーション
+            .animation(.default)
     }
 }
