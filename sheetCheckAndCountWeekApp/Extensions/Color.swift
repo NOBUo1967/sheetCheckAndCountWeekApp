849206a0e8125a774ec7069dba9308153b29c463
Shin H <nobunegi13@gmail.com>
2022-01-15 07:02:46 +0900

    [update]定義したColorを一元管理できるようにColorにextensionを追加した

---
+++ sheetCheckAndCountWeekApp/Extensions/Color.swift
@@ -0,0 +1,13 @@
+//
+//  Color.swift
+//  sheetCheckAndCountWeekApp
+//
+//  Created by Shinichiro Hirasawa on 2022/01/15.
+//
+
+import Foundation
+import SwiftUI
+
+extension Color {
+    static let customBeige = Color("customBeige")
+}
