60e8662e39050f13822ee28f1782549b8268ce83
Shin H <nobunegi13@gmail.com>
2022-01-15 07:54:18 +0900

    [update]日数計算画面に背景色を設定
    darkモードにすると非選択のtabのiconの色が消えてしまう。実機でテストすると設定した色にならない。

--- sheetCheckAndCountWeekApp/Views/DaysCount.swift
+++ sheetCheckAndCountWeekApp/Views/DaysCount.swift
@@ -16,38 +16,46 @@ struct DaysCount: View {
     @State private var includestartDate = false
     
     var body: some View {
-        Form {
-            // 日付を選択する領域
-            Section {
-                DatePicker("開始日", selection: $startDate, displayedComponents: .date)
-                    .environment(\.locale, Locale(identifier: "ja_JP"))
-                
-                DatePicker("終了日", selection: $endDate, displayedComponents: .date)
-                    .environment(\.locale, Locale(identifier: "ja_JP"))
-                
-                // 開始日を含むか否かのtoggleボタン
-                Toggle(isOn: $includestartDate) {
-                    Text("開始日を含む")
-                }
-                
-            }  // Section
-            
-            // 計算結果を表示する領域
-            Section{
-                HStack {
-                    // 文字サイズが小さすぎるためtitle2を指定
-                    Text("日数")
-                        .font(.title2)
-                    // 両端揃えのためにSpacerを挿入
-                    Spacer()
-                    // 計算結果を目立たせるために日数部分は赤字にした
-                    Text("\(DateCalculator().calclateSpan(startDate: self.startDate, endDate: self.endDate, includestartDate: self.includestartDate))日")
-                        .font(.title2)
-                        .foregroundColor(.red)
+        // 背景色を設定するためにZStackを設置
+        ZStack {
+            // 背景色の設定
+            Color.customBeige
+                .edgesIgnoringSafeArea(.all)
+            Form {
+                // 日付を選択する領域
+                Section {
+                    DatePicker("開始日", selection: $startDate, displayedComponents: .date)
+                        .environment(\.locale, Locale(identifier: "ja_JP"))
                     
-                } // HStack
-            } // Section
-        } // Form
+                    DatePicker("終了日", selection: $endDate, displayedComponents: .date)
+                        .environment(\.locale, Locale(identifier: "ja_JP"))
+                    
+                    // 開始日を含むか否かのtoggleボタン
+                    Toggle(isOn: $includestartDate) {
+                        Text("開始日を含む")
+                    }
+                }  // Section
+                
+                // 計算結果を表示する領域
+                Section{
+                    HStack {
+                        // 文字サイズが小さすぎるためtitle2を指定
+                        Text("日数")
+                            .font(.title2)
+                        // 両端揃えのためにSpacerを挿入
+                        Spacer()
+                        // 計算結果を目立たせるために日数部分は赤字にした
+                        Text("\(DateCalculator().calclateSpan(startDate: self.startDate, endDate: self.endDate, includestartDate: self.includestartDate))日")
+                            .font(.title2)
+                            .foregroundColor(.red)
+                    } // HStack
+                } // Section
+            } // Form
+            // 設定した背景色を有効にするためにFormのデフォルトの背景色を透明にした
+            .onAppear() {
+                UITableView.appearance().backgroundColor = .clear
+            }
+        } // ZStack
     } // body
 }
 
