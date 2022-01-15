cb405f301f7aabd2b15cda00f811cd05fed7b79a
Shin H <nobunegi13@gmail.com>
2022-01-15 10:46:00 +0900

    [update]各画面に背景色を設定した

--- sheetCheckAndCountWeekApp/Views/LeftoverMedicineCount.swift
+++ sheetCheckAndCountWeekApp/Views/LeftoverMedicineCount.swift
@@ -49,106 +49,114 @@ struct LeftoverMedicineCount: View {
     }
     
     var body: some View {
-        Form {
-            Section {
-                // 処方日の入力
-                DatePicker("内服開始日", selection: self.$startDate, displayedComponents: .date)
-                    .environment(\.locale, Locale(identifier: "ja_JP"))
-                
-                // 処方日数入力部分
-                HStack {
-                    // TextFieldのラベルとしてTextを設置
-                    Text("処方日数")
-                    // 両端揃えにするためにSpacerを設置
-                    Spacer()
-                    // 日数入力部分
-                    TextField("日分", text: self.$prescriptionDays)
-                        .textFieldStyle(RoundedBorderTextFieldStyle())
-                        .keyboardType(.numberPad)
-                    // DatePickerと同じ幅にするためにwidth:110とした
-                        .frame(width: 110, height: 30)
-                } // HStack
-                
-                // 中断日の入力
-                DatePicker("中断日", selection: self.$interruptionDate, displayedComponents: .date)
-                    .environment(\.locale, Locale(identifier: "ja_JP"))
-                
-                // 計算ボタン
-                HStack {
-                    // Buttonを右端寄せにするためにSpacerを設置した
-                    Spacer()
-                    // 残薬計算ボタン
-                    Button(action: {
-                        // 入力された処方日数はString型のためキャスト。後にif let文でnilチェックするためInt?型にする
-                        let prescriptionDays: Int? = Int(self.prescriptionDays)
-                        // 内服開始日と内服中断日の差を計算
-                        // 内服開始日を計算に含むためincludestartDateはtrueとする
-                        let dateSpan: Int = DateCalculator().calclateSpan(startDate: self.startDate, endDate: self.interruptionDate, includestartDate: true)
-                        
-                        // 日数の差がマイナスになる場合はアラートを表示するして処理を抜ける
-                        if dateSpan < 0 {
-                            alertType = .incorrectSelectedDate
-                            showAlert = true
-                            return
-                        }
-                        
-                        // 処方日数のnilチェック。0は入力されうるためチェックしない
-                        if let prescriptionDays = prescriptionDays {
-                            // 残薬数の計算
-                            numberOfDaysLeftoverMedicines = prescriptionDays - dateSpan
+        // 背景色を設定するためにZStackを設置
+        ZStack {
+            // 背景色の設定
+            Color.customBeige
+                .edgesIgnoringSafeArea(.all)
+            Form {
+                Section {
+                    // 処方日の入力
+                    DatePicker("内服開始日", selection: self.$startDate, displayedComponents: .date)
+                        .environment(\.locale, Locale(identifier: "ja_JP"))
+                    
+                    // 処方日数入力部分
+                    HStack {
+                        // TextFieldのラベルとしてTextを設置
+                        Text("処方日数")
+                        // 両端揃えにするためにSpacerを設置
+                        Spacer()
+                        // 日数入力部分
+                        TextField("日分", text: self.$prescriptionDays)
+                            .textFieldStyle(RoundedBorderTextFieldStyle())
+                            .keyboardType(.numberPad)
+                        // DatePickerと同じ幅にするためにwidth:110とした
+                            .frame(width: 110, height: 30)
+                    } // HStack
+                    
+                    // 中断日の入力
+                    DatePicker("中断日", selection: self.$interruptionDate, displayedComponents: .date)
+                        .environment(\.locale, Locale(identifier: "ja_JP"))
+                    
+                    // 計算ボタン
+                    HStack {
+                        // Buttonを右端寄せにするためにSpacerを設置した
+                        Spacer()
+                        // 残薬計算ボタン
+                        Button(action: {
+                            // 入力された処方日数はString型のためキャスト。後にif let文でnilチェックするためInt?型にする
+                            let prescriptionDays: Int? = Int(self.prescriptionDays)
+                            // 内服開始日と内服中断日の差を計算
+                            // 内服開始日を計算に含むためincludestartDateはtrueとする
+                            let dateSpan: Int = DateCalculator().calclateSpan(startDate: self.startDate, endDate: self.interruptionDate, includestartDate: true)
                             
-                            // 残薬数がマイナスになる場合は内服できていなかったかもしれない旨のアラートを表示する
-                            if numberOfDaysLeftoverMedicines < 0 {
-                                alertType = .runOutMedicine
+                            // 日数の差がマイナスになる場合はアラートを表示するして処理を抜ける
+                            if dateSpan < 0 {
+                                alertType = .incorrectSelectedDate
+                                showAlert = true
+                                return
+                            }
+                            
+                            // 処方日数のnilチェック。0は入力されうるためチェックしない
+                            if let prescriptionDays = prescriptionDays {
+                                // 残薬数の計算
+                                numberOfDaysLeftoverMedicines = prescriptionDays - dateSpan
+                                
+                                // 残薬数がマイナスになる場合は内服できていなかったかもしれない旨のアラートを表示する
+                                if numberOfDaysLeftoverMedicines < 0 {
+                                    alertType = .runOutMedicine
+                                    showAlert = true
+                                }
+                            } else {
+                                // 処方日数に値が入力されていない場合はアラートを表示する
+                                alertType = .incorrectInputValue
                                 showAlert = true
                             }
-                        } else {
-                            // 処方日数に値が入力されていない場合はアラートを表示する
-                            alertType = .incorrectInputValue
-                            showAlert = true
-                        }
-                    }) {
-                        Text("計算")
-                    } //Button
-                    .buttonStyle(PrimaryButtonStyle())
-                } // HStack
-            } // Section
-            
-            
-            // 計算結果表示部分
-            Section {
-                VStack {
-                    Text("\(startDate, formatter: dateFormat)から")
-                    Text("\(interruptionDate, formatter: dateFormat)まで")
-                    Text("内服している場合")
-                    (Text("残薬は")
-                     + Text("\(numberOfDaysLeftoverMedicines)日分").foregroundColor(Color.red)
-                     + Text("あると考えられます"))
-                } // VStack
-                // Sction内のVStackの要素がTextの幅と同値であったためmaxWidthをinfinityとし、その上でcenterで中央寄せにした
-                .frame(maxWidth:.infinity, alignment: .center)
-                .font(.title3)
-            } // Section
-        }// Form
-        // keyboard外をタップするとkeyboardを閉じる処理
-        .onTapGesture {
-            UIApplication.shared.closeKeyboard()
-        }
-        // Alert部分
-        .alert(isPresented: $showAlert) {
-            switch alertType {
-                // 入力値が誤っているケース
-            case .incorrectInputValue:
-                return Alert(title: Text("正しい値を入力してください"), message: Text("処方日数には0以上の数字を入力してください"), dismissButton: .default(Text("OK")))
-                // 中断日が内服開始日より前の日付になっているケース
-            case .incorrectSelectedDate:
-                return Alert(title: Text("正しい値を入力してください"), message: Text("中断日は内服開始日より後の日付を選択してください"), dismissButton: .default(Text("OK")))
-                // 残薬数がマイナスになるケース
-            case .runOutMedicine:
-                return Alert(title: Text("処方日数が足りていません"), message: Text("何日間か内服できていなかった\n可能性があります"), dismissButton: .default(Text("OK")))
+                        }) {
+                            Text("計算")
+                        } //Button
+                        .buttonStyle(PrimaryButtonStyle())
+                    } // HStack
+                } // Section
+                
+                // 計算結果表示部分
+                Section {
+                    VStack {
+                        Text("\(startDate, formatter: dateFormat)から")
+                        Text("\(interruptionDate, formatter: dateFormat)まで")
+                        Text("内服している場合")
+                        (Text("残薬は")
+                         + Text("\(numberOfDaysLeftoverMedicines)日分").foregroundColor(Color.red)
+                         + Text("あると考えられます"))
+                    } // VStack
+                    // Sction内のVStackの要素がTextの幅と同値であったためmaxWidthをinfinityとし、その上でcenterで中央寄せにした
+                    .frame(maxWidth:.infinity, alignment: .center)
+                    .font(.title3)
+                } // Section
+            }// Form
+            // 設定した背景色を有効にするためにFormのデフォルトの背景色を透明にした
+            .onAppear() {
+                UITableView.appearance().backgroundColor = .clear
             }
-            
-        }
+            // keyboard外をタップするとkeyboardを閉じる処理
+            .onTapGesture {
+                UIApplication.shared.closeKeyboard()
+            }
+            // Alert部分
+            .alert(isPresented: $showAlert) {
+                switch alertType {
+                    // 入力値が誤っているケース
+                case .incorrectInputValue:
+                    return Alert(title: Text("正しい値を入力してください"), message: Text("処方日数には0以上の数字を入力してください"), dismissButton: .default(Text("OK")))
+                    // 中断日が内服開始日より前の日付になっているケース
+                case .incorrectSelectedDate:
+                    return Alert(title: Text("正しい値を入力してください"), message: Text("中断日は内服開始日より後の日付を選択してください"), dismissButton: .default(Text("OK")))
+                    // 残薬数がマイナスになるケース
+                case .runOutMedicine:
+                    return Alert(title: Text("処方日数が足りていません"), message: Text("何日間か内服できていなかった\n可能性があります"), dismissButton: .default(Text("OK")))
+                } // switch
+            } // alert
+        } // ZStack
     } // body
 }
 
--- sheetCheckAndCountWeekApp/Views/SheetCount.swift
+++ sheetCheckAndCountWeekApp/Views/SheetCount.swift
@@ -23,88 +23,98 @@ struct SheetCount: View {
     @State var showAlert:Bool = false
     
     var body: some View {
-        Form {
-            Section {
-                HStack {
-                    Text("1日必要錠(包)数")
-                    // 両端寄せにするためにスペースを挿入
-                    Spacer()
-                    TextField("錠(包)", text: self.$dairyDose)
-                        .textFieldStyle(RoundedBorderTextFieldStyle())
-                        .keyboardType(.decimalPad)
-                        .frame(width: 110, height: 30)
-                } // HStack
+        // 背景色を設定するためにZStackを設置
+        ZStack {
+            // 背景色の設定
+            Color.customBeige
+                .edgesIgnoringSafeArea(.all)
+            Form {
+                Section {
+                    HStack {
+                        Text("1日必要錠(包)数")
+                        // 両端寄せにするためにスペースを挿入
+                        Spacer()
+                        TextField("錠(包)", text: self.$dairyDose)
+                            .textFieldStyle(RoundedBorderTextFieldStyle())
+                            .keyboardType(.decimalPad)
+                            .frame(width: 110, height: 30)
+                    } // HStack
+                    
+                    HStack {
+                        Text("処方日数")
+                        // 両端寄せにするためにスペースを挿入
+                        Spacer()
+                        TextField("日", text: self.$numberOfDay)
+                            .textFieldStyle(RoundedBorderTextFieldStyle())
+                            .keyboardType(.numberPad)
+                            .frame(width: 110, height: 30)
+                    } // HStack
+                    
+                    HStack {
+                        Text("1シート(つづり)\nあたりの錠(包)数")
+                        // 両端寄せにするためにスペースを挿入
+                        Spacer()
+                        TextField("錠(包)", text: self.$numberPerSheet)
+                            .textFieldStyle(RoundedBorderTextFieldStyle())
+                            .keyboardType(.numberPad)
+                            .frame(width: 110, height: 30)
+                    } // HStack
+                    
+                    // 計算ボタン
+                    HStack {
+                        // Buttonを右端寄せにするためにSpacerを設置した
+                        Spacer()
+                        Button(action: {
+                            // キャストしてOptional型にしておかないとif let文で、nilチェックできないため、ここで値をDouble型にキャストしておく
+                            let dairyDoseDouble:Double? = Double(dairyDose)
+                            let numberOfDayDouble:Double? = Double(numberOfDay)
+                            let numberPerSheetDouble: Double? = Double(numberPerSheet)
+                            // 各値のnilチェックと空文字チェック
+                            // 「 8」などスペース始まりの文字は弾いていないが、キーボードをNamPadに指定しているため問題ないと考える。
+                            if let dairyDose = dairyDoseDouble, let numberOfDay = numberOfDayDouble, let numberPerSheet = numberPerSheetDouble, numberPerSheet != 0 {
+                                // 必要ヒート数を計算
+                                self.total = Int(dairyDose * numberOfDay / numberPerSheet)
+                                // 端数の錠数を計算。小数点第3位で四捨五入する
+                                self.fraction = round((dairyDose * numberOfDay).truncatingRemainder(dividingBy: numberPerSheet)*100) / 100
+                            } else {
+                                showAlert = true
+                            }
+                        }) {
+                            Text("計算")
+                        } //Button
+                        .buttonStyle(PrimaryButtonStyle())
+                    } // HStack
+                } // Section
                 
-                HStack {
-                    Text("処方日数")
-                    // 両端寄せにするためにスペースを挿入
-                    Spacer()
-                    TextField("日", text: self.$numberOfDay)
-                        .textFieldStyle(RoundedBorderTextFieldStyle())
-                        .keyboardType(.numberPad)
-                        .frame(width: 110, height: 30)
-                } // HStack
-                
-                HStack {
-                    Text("1シート(つづり)\nあたりの錠(包)数")
-                    // 両端寄せにするためにスペースを挿入
-                    Spacer()
-                    TextField("錠(包)", text: self.$numberPerSheet)
-                        .textFieldStyle(RoundedBorderTextFieldStyle())
-                        .keyboardType(.numberPad)
-                        .frame(width: 110, height: 30)
-                } // HStack
-                
-                // 計算ボタン
-                HStack {
-                    // Buttonを右端寄せにするためにSpacerを設置した
-                    Spacer()
-                    Button(action: {
-                        // キャストしてOptional型にしておかないとif let文で、nilチェックできないため、ここで値をDouble型にキャストしておく
-                        let dairyDoseDouble:Double? = Double(dairyDose)
-                        let numberOfDayDouble:Double? = Double(numberOfDay)
-                        let numberPerSheetDouble: Double? = Double(numberPerSheet)
-                        // 各値のnilチェックと空文字チェック
-                        // 「 8」などスペース始まりの文字は弾いていないが、キーボードをNamPadに指定しているため問題ないと考える。
-                        if let dairyDose = dairyDoseDouble, let numberOfDay = numberOfDayDouble, let numberPerSheet = numberPerSheetDouble, numberPerSheet != 0 {
-                            // 必要ヒート数を計算
-                            self.total = Int(dairyDose * numberOfDay / numberPerSheet)
-                            // 端数の錠数を計算。小数点第3位で四捨五入する
-                            self.fraction = round((dairyDose * numberOfDay).truncatingRemainder(dividingBy: numberPerSheet)*100) / 100
-                        } else {
-                            showAlert = true
-                        }
-                    }) {
-                        Text("計算")
-                    } //Button
-                    .buttonStyle(PrimaryButtonStyle())
-                } // HStack
-            } // Section
-            
-            // 計算結果表示部分
-            Section {
-                VStack {
-                    // 端数は小数点第二位まで表示する。1回0.25錠までが現実的なところ。
-                    (Text("\(self.total)").foregroundColor(Color.red)
-                        + Text("シート(つづり)"))
-                    (Text("+")
-                        + Text("\(String(format: "%.2f", self.fraction))").foregroundColor(Color.red)
-                        + Text("錠(包)"))
-                    Text("必要です")
-                }// VStack
-                // Sction内のVStackの要素がTextの幅と同値であったためmaxWidthをinfinityとし、その上でcenterで中央寄せにした
-                .frame(maxWidth:.infinity, alignment: .center)
-                .font(.title2)
-            } // Section
-        } // Form
-        // keyboard外をタップするとkeyboardを閉じる処理
-        .onTapGesture {
-            UIApplication.shared.closeKeyboard()
-        }
-        // 
-        .alert(isPresented: $showAlert) {
-            Alert(title: Text("正しい値を入力してください"), message: Text("入力できる値は数値のみです\n1シートあたりの錠数に０は入力できません"), dismissButton: .default(Text("OK")))
-        }
+                // 計算結果表示部分
+                Section {
+                    VStack {
+                        // 端数は小数点第二位まで表示する。1回0.25錠までが現実的なところ。
+                        (Text("\(self.total)").foregroundColor(Color.red)
+                         + Text("シート(つづり)"))
+                        (Text("+")
+                         + Text("\(String(format: "%.2f", self.fraction))").foregroundColor(Color.red)
+                         + Text("錠(包)"))
+                        Text("必要です")
+                    }// VStack
+                    // Sction内のVStackの要素がTextの幅と同値であったためmaxWidthをinfinityとし、その上でcenterで中央寄せにした
+                    .frame(maxWidth:.infinity, alignment: .center)
+                    .font(.title2)
+                } // Section
+            } // Form
+            // 設定した背景色を有効にするためにFormのデフォルトの背景色を透明にした
+            .onAppear() {
+                UITableView.appearance().backgroundColor = .clear
+            }
+            // keyboard外をタップするとkeyboardを閉じる処理
+            .onTapGesture {
+                UIApplication.shared.closeKeyboard()
+            }
+            //
+            .alert(isPresented: $showAlert) {
+                Alert(title: Text("正しい値を入力してください"), message: Text("入力できる値は数値のみです\n1シートあたりの錠数に０は入力できません"), dismissButton: .default(Text("OK")))
+            }
+        } // ZStack
     } // body
 }
 
