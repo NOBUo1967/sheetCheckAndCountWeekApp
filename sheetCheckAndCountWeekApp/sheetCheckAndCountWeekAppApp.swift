c701ee3d79aedcb0e2f609f31125be851a6ea363
Shin H <nobunegi13@gmail.com>
2022-01-17 09:26:27 +0900

    [update]広告の初期化処理を行った

--- sheetCheckAndCountWeekApp/sheetCheckAndCountWeekAppApp.swift
+++ sheetCheckAndCountWeekApp/sheetCheckAndCountWeekAppApp.swift
@@ -6,9 +6,24 @@
 //
 
 import SwiftUI
+// AdMobを導入
+import UIKit
+import GoogleMobileAds
+
+// ApDelegateクラスの定義
+class AppDelegate: NSObject, UIApplicationDelegate {
+    func application(_ application: UIApplication, didFinishLanchingWithOptions lanchOptions: [UIApplication.LaunchOptionsKey: Any]? = nil) -> Bool {
+        // Mobile_Ads_SDKの初期化
+        GADMobileAds.sharedInstance().start(completionHandler: nil)
+        return true
+    }
+}
 
 @main
 struct sheetCheckAndCountWeekAppApp: App {
+    // SwiftUIのライフサイクルにAppDelegateクラスを追加
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
