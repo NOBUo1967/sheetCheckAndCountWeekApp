d6037f63fe5c9b6bac5a893ae11254a9c7da526a
NOBUo <66336936+NOBUo1967@users.noreply.github.com>
2022-04-25 06:59:29 +0900

    Update BannerAdView.swift

--- sheetCheckAndCountWeekApp/Views/BannerAdView.swift
+++ sheetCheckAndCountWeekApp/Views/BannerAdView.swift
@@ -11,7 +11,7 @@ import GoogleMobileAds
 struct BannerAdView: UIViewRepresentable {
     func makeUIView(context: Context) -> GADBannerView {
         let banner = GADBannerView(adSize: GADAdSizeBanner)
-        banner.adUnitID = "ca-app-pub-6680256433251064/7520320269" // 本番広告用のID
+        banner.adUnitID = {adMobKey} // adMob広告IDを取得して入力
         banner.rootViewController = UIApplication.shared.windows.first?.rootViewController
         banner.load(GADRequest())
         return banner
